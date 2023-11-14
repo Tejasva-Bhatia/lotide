0b7273eedbcae6cdefc5cf49aeb612d5e2e62308
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-13 21:25:05 -0600

    Made assertEqual function of my own

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected)
+    return console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  if (actual === expected)
+    return console.log("✅✅✅ Assertion Passed: " + actual + " !== " + expected);
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bhatia", "Tejasva");
+assertEqual("Tejasva", "Tejasva");
+assertEqual(4, 4);
+assertEqual(6, 5);
