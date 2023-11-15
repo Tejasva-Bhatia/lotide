873e1d4130d4a12c4c8761efb0b3cfbf3a4e8650
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-15 15:14:22 -0600

    CountLetters completed

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected)
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===  ${expected}`);
+
+};
+
+const countLetters = function(inputString) {
+  let result = {};
+  for (let i = 0; i < inputString.length; i++) {
+    let currentLetter = inputString[i];
+    if (result[currentLetter])
+      result[currentLetter]++;
+    
+    else
+      result[currentLetter] = 1;
+  }
+  return result;
+};
+
+const result = countLetters('LHL');
+console.log(result);
+assertEqual(result['L'],2);
+assertEqual(result['H'],1);
