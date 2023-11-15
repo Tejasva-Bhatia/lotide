b94f8b6eb4ee8dedc8d7688fc5b474231002ab47
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-14 20:08:55 -0600

    minor change

--- without.js
+++ without.js
@@ -15,7 +15,7 @@ const eqArrays = function(arr1, arr2) {
 const assertArrayEqual = function(array1, array2) {
   if (!(eqArrays(array1, array2)))
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  if (eqArrays(array1, array2))
+  else
     console.log(`✅✅✅ Assertion Passed:  ${array1}  ===  ${array2}`);
 
 };
