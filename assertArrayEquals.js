db3dc824d2d62c534dc08d9e81f7705a5580cd7f
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-14 19:36:05 -0600

    assertArrayEquals code improved

--- assertArrayEquals.js
+++ assertArrayEquals.js
@@ -14,11 +14,12 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArrayEqual = function(array1, array2) {
   if (!(eqArrays(array1, array2)))
-    return console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   if (eqArrays(array1, array2))
-    return console.log(`✅✅✅ Assertion Passed:  ${array1}  ===  ${array2}`);
+    console.log(`✅✅✅ Assertion Passed:  ${array1}  ===  ${array2}`);
 
 };
+
 
 
 
