08be3e433d8d45bae1c1b0a936971e2d676d18a8
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-16 18:55:16 -0600

    Corrected the code

--- takeUntil.js
+++ takeUntil.js
@@ -19,28 +19,18 @@ const assertArrayEqual = function(array1, array2) {
 
 };
 
+
 const takeUntil = function(array, callback) {
   let ans = [];
-  array.forEach(element => {
-    if (callback(element)) {
-      return; // Use 'return' to exit the callback function, not 'break'
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      break; //
     }
-    ans.push(element);
-  });
+    ans.push(array[i]);
+  }
   return ans;
 };
 
-// const takeUntil = function(array, callback) {
-//   let ans = [];
-//   for (let i = 0; i < array.length; i++) {
-//     if (callback(array[i])) {
-//       break; //
-//     }
-//     ans.push(array[i]);
-//   }
-//   return ans;
-// };
-
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-assertArrayEqual(results1, [1, 2, 5, 7, 2, 2, 4, 5]);
+assertArrayEqual(results1, [1, 2, 5, 7, 2]);
