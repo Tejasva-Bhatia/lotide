48dd468ca9df5dd635d57fb1d72aa209a17b2d2a
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-16 18:51:44 -0600

    Implemented takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(array1, array2) {
+  if (!(eqArrays(array1, array2)))
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  if (eqArrays(array1, array2))
+    console.log(`✅✅✅ Assertion Passed:  ${array1}  ===  ${array2}`);
+
+};
+
+const takeUntil = function(array, callback) {
+  let ans = [];
+  array.forEach(element => {
+    if (callback(element)) {
+      return; // Use 'return' to exit the callback function, not 'break'
+    }
+    ans.push(element);
+  });
+  return ans;
+};
+
+// const takeUntil = function(array, callback) {
+//   let ans = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (callback(array[i])) {
+//       break; //
+//     }
+//     ans.push(array[i]);
+//   }
+//   return ans;
+// };
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArrayEqual(results1, [1, 2, 5, 7, 2, 2, 4, 5]);
