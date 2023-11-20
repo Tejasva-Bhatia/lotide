7f2822022e7cca3aafba2b455bde02b6d969bbf5
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-20 13:52:24 -0600

    test file created for assertEqual.js and head.js

--- assertEqual.js
+++ assertEqual.js
@@ -5,11 +5,4 @@ const assertEqual = function(actual, expected) {
     return console.log(`✅✅✅ Assertion Passed:  ${actual}  ===  ${expected}`);
 
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bhatia", "Tejasva");
-assertEqual("Tejasva", "Tejasva");
-assertEqual(4, 4);
-assertEqual(6, 5);
+module.exports = assertEqual;
