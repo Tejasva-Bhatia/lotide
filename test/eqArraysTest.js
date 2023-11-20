88ca1ca557fd65dbd9f29bc326af093d74384519
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-20 14:28:07 -0600

    added few more test cases

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,3 +2,9 @@ const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays([1, 2, 3], [4, 5, 6]), false);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
