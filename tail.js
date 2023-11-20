b161e49a9fa371e64698e376251fc6119e374d5e
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-20 14:16:20 -0600

    tailTest.js implemented

--- tail.js
+++ tail.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected)
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  if (actual === expected)
-    return console.log(`✅✅✅ Assertion Passed:  ${actual}  ===  ${expected}`);
-
-};
 
 const tail = function(arr) {
   const newArray = arr.slice(1);
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
